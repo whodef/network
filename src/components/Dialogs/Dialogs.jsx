5541199b938ee8c497c3d440c524b8e9f4438293
whodef <tatyana@selyuk.com>
2020-10-09 18:25:59 +0300

    Added columns in Dialogs; Added Dialogs.module.css;

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -3,8 +3,27 @@ import s from './Dialogs.module.css';
 
 const Dialogs = () => {
     return (
-        <div>
-            Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <div className={s.dialog + ' ' + s.active}>
+                    Elizabeth
+                </div>
+                <div className={s.dialog}>
+                    Tom Riddle
+                </div>
+                <div className={s.dialog}>
+                    Billy
+                </div>
+                <div className={s.dialog}>
+                    Trump
+                </div>
+            </div>
+
+            <div className={s.messages}>
+                <div className={s.message}>Hi</div>
+                <div className={s.message}>How are you?</div>
+                <div className={s.message}>Yo!</div>
+            </div>
         </div>
     );
 }
